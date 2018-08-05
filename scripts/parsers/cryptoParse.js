9452a6c4a836bdd0ee70fc7a539ad297bbcaa976
Chris Wininger <cwininger@sonatype.com>
2018-08-05 09:14:33 -0400

    added duration

--- scripts/parsers/cryptoParse.js
+++ scripts/parsers/cryptoParse.js
@@ -5,14 +5,14 @@ module.exports = function processPage(html) {
   const $ = cheerio.load(html)
 
   const dayMap = [
-      new Date('8/10/2018'),
-      new Date('8/11/2018'),
-      new Date('8/12/2018')
+    new Date('2018-08-10T00:00:00.000Z'), // Friday
+    new Date('2018-08-11T00:00:00.000Z'), // Saturday
+    new Date('2018-08-12T00:00:00.000Z'), // Sunday
   ]
 
   const getHours = (strTime) => {
     const hour = parseInt(strTime.split(':')[0])
-    if (strTime.toLowerCase().indexOf('pm') >= 0) {
+    if (strTime.toLowerCase().indexOf('pm') >= 0 && hour != 12) {
       // pm to military time
       return hour + 12
     }
@@ -21,7 +21,14 @@ module.exports = function processPage(html) {
     return hour
   }
 
-  const getMinutes = (strTime) => parseInt(strTime.split(':')[1].toLowerCase().replace('am', '').replace('pm', ''))
+  const getMinutes = (strTime) => {
+    if (strTime.indexOf(':') <= 0) {
+      // using shorthand 12pm, no minutes
+      return 0
+    }
+
+    return parseInt(strTime.split(':')[1].toLowerCase().replace('am', '').replace('pm', ''))
+  }
 
   $('div.day')
     .each((ndxDay, elDay) => {
@@ -29,19 +36,30 @@ module.exports = function processPage(html) {
 
        $(elDay).children('.event').each((ndxEvent, elTalk) => {
          const strTime = $(elTalk).children('.event-summary').children('.event-time').text().trim()
+         const strEndTime = $(elTalk).children('.event-summary').children('.event-time')[0].attribs.alt
+             .split(',')[2].split('–')[1].trim()
          const hours = getHours(strTime)
          const minutes = getMinutes(strTime)
+         const endHours = getHours(strEndTime)
+         const endMinutes = getMinutes(strEndTime)
+         const duration = (endHours - hours) * 60 + (endMinutes - minutes)
+
          const time = new Date(day)
-         time.setHours(hours)
-         time.setMinutes(minutes)
+
+         /*
+            Seriously, setHours converts to local time regardless of original date time zone, use setUTCHours or
+             you will got mad, JavaScript Date/Time BS is insane!
+          */
+         time.setUTCHours(hours, minutes)
 
          talks = [
               ...talks,
               {
-                time: `${time.toUTCString()}`,
                 title: $(elTalk).children('.event-summary').children('.title-wrapper').text().trim(),
                 location: 'Crypto Village',
-                speaker: ''
+                speaker: '',
+                time: `${time.toUTCString()}`,
+                duration: duration,
               }]
         })
     })
